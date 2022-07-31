93701edca0de666fc200417e71be1eafb99b1029
Orest Maryniuk <98083499+ormar90@users.noreply.github.com>
2022-07-31 10:14:08 +0300

    finished first example

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const makeGalleryItemMarkup = galleryItems.map(({ description, original, preview }) => {
+    return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </div>`
+}).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', makeGalleryItemMarkup);
+
+galleryEl.addEventListener('click', onClickImg);
+
+function onClickImg(e) {
+    e.preventDefault();
+    if (!e.target.dataset.source) {
+        return;
+    }
+    
+    const instance = basicLightbox.create(`<img src = "${e.target.dataset.source}">`, {
+        onShow: (instance) => {
+            window.addEventListener('keydown', onKlickEscape);
+            // console.log('onShow', instance)
+        },
+        onClose: (instance) => {
+            window.removeEventListener('keydown', onKlickEscape);
+            // console.log('onClose', instance)
+        }
+    });
+        instance.show();
+
+    function onKlickEscape(e) {
+        instance.close();
+    }
+}
+
+
